2b5f416cf860674ddd038b5056c374d3eab1ad30
Hashim Gari <hashmaster3k@gmail.com>
2020-07-14 09:04:17 -0600

    Adds test for existance of object

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -1,2 +1,12 @@
-class ApartmentTest
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class ApartmentTest < Minitest::Test
+
+  def initialize
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal Apartment, unit1
+  end
+
 end
