70804bde83834e6dc97518b3e75964bb2b34b378
Hashim Gari <hashmaster3k@gmail.com>
2020-07-14 09:10:07 -0600

    Adds testing for unit number

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -12,4 +12,10 @@ class ApartmentTest < Minitest::Test
     assert_instance_of Apartment, unit1
   end
 
+  def test_unit_number
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal "A1", unit1.number
+  end
+
 end
