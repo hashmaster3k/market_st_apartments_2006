6fa59f7f6076686bf6235845f3ff619eb56d9f25
Hashim Gari <hashmaster3k@gmail.com>
2020-07-14 09:20:09 -0600

    Adds test to see if apartment is being rented

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -34,4 +34,10 @@ class ApartmentTest < Minitest::Test
     assert_equal 1, unit1.bedrooms
   end
 
+  def test_if_rented
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal nil, unit1.renter
+  end
+
 end
