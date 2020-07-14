82e0ca04166a5ed3b2695430c9b9242ad79b0527
Hashim Gari <hashmaster3k@gmail.com>
2020-07-14 09:17:33 -0600

    Adds test to ensure object exists for renter

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -2,4 +2,11 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class RenterTest < Minitest ::Test
+
+  def test_it_exists
+    renter1 = Renter.new("Jessie")
+
+    assert_instance_of Renter, renter1
+  end
+
 end
