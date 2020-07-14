0f90e155224688bb6ff9d0dba5cc3d306803a7c0
Hashim Gari <hashmaster3k@gmail.com>
2020-07-14 09:15:04 -0600

    Adds test to obtain number of bedrooms

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -28,4 +28,10 @@ class ApartmentTest < Minitest::Test
     assert_equal 1, unit1.bathrooms
   end
 
+  def test_gets_bedrooms
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal 1, unit1.bedrooms
+  end
+
 end
